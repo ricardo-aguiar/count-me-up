40e6724177cdbe38bf799ca02b28920088ebe0e2
ricardo-aguiar <ric.aguiar@me.com>
2017-07-22 16:28:04 +0100

    Initial project structure with Spring Boot application, pom, test dependencies and initial API (#1)

---
+++ count-me-up-service/src/main/java/tech/experimental/countmeup/service/CountMeUpApplication.java
@@ -0,0 +1,20 @@
+package tech.experimental.countmeup.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot main application entry point
+ */
+@SpringBootApplication
+public class CountMeUpApplication
+{
+    /**
+     * Main method.
+     * @param args
+     */
+    public static void main(final String[] args)
+    {
+        SpringApplication.run(CountMeUpApplication.class, args);
+    }
+}
